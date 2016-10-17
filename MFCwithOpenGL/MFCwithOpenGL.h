2d4b98d7207eea9ba039dedab3b79f4c3c350d0e
Alex Carmichael <carmichael.alexander@gmail.com>
2016-10-17 10:40:53 -0700

    Add project files.

---
+++ MFCwithOpenGL/MFCwithOpenGL.h
@@ -0,0 +1,32 @@
+
+// MFCwithOpenGL.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCwithOpenGLApp:
+// See MFCwithOpenGL.cpp for the implementation of this class
+//
+
+class CMFCwithOpenGLApp : public CWinApp
+{
+public:
+	CMFCwithOpenGLApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCwithOpenGLApp theApp;
